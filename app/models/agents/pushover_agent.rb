d641b48f3cbf39fe2719e30a862d92dec14553a8
Linode <linode@eagerdesigns.com.au>
2024-02-29 04:02:03 +0000

    Update pushover to support image attachments

--- app/models/agents/pushover_agent.rb
+++ app/models/agents/pushover_agent.rb
@@ -1,3 +1,6 @@
+require 'httmultiparty'
+require 'cgi'
+
 module Agents
   class PushoverAgent < Agent
     can_dry_run!
@@ -26,6 +29,7 @@ module Agents
       * `title` or `subject` - your notification's title
       * `url` - a supplementary URL to show with your message - `512` Character Limit
       * `url_title` - a title for your supplementary URL, otherwise just the URL is shown - `100` Character Limit
+      * `image_url` - a URL to an image file to be sent with the message
       * `timestamp` - a [Unix timestamp](https://en.wikipedia.org/wiki/Unix_time) of your message's date and time to display to the user, rather than the time your message is received by the Pushover API.
       * `priority` - send as `-1` to always send as a quiet notification, `0` is default, `1` to display as high-priority and bypass the user's quiet hours, or `2` for emergency priority: [Please read Pushover Docs on Emergency Priority](https://pushover.net/api#priority)
       * `sound` - the name of one of the sounds supported by device clients to override the user's default sound choice. [See PushOver docs for sound options.](https://pushover.net/api#sounds)
@@ -44,6 +48,7 @@ module Agents
         'title' => '{{ title }}',
         'url' => '{{ url }}',
         'url_title' => '{{ url_title }}',
+        'image_url' => '{{ image_url }}',
         'priority' => '{{ priority }}',
         'timestamp' => '{{ timestamp }}',
         'sound' => '{{ sound }}',
@@ -64,6 +69,7 @@ module Agents
       incoming_events.each do |event|
         interpolate_with(event) do
           post_params = {}
+          attachment = nil
 
           # required parameters
           %w[
@@ -109,7 +115,30 @@ module Agents
               end
           end
 
-          send_notification(post_params)
+          # Process image_url option
+          if image_url = String.try_convert(interpolated['image_url'].presence)
+            begin
+              attachment = open(image_url)
+            rescue => e
+              log "Failed to download image from '#{image_url}': #{e.message}"
+            end
+
+            # Check if the attachment size exceeds the allowed limit
+            if attachment && attachment.size > 2_621_440
+              log "Image size exceeds 2.5 MB limit for '#{image_url}'. Skipping attachment."
+              attachment.close
+              attachment = nil
+            end
+
+            # Check if the attachment is an image of a supported type
+            if attachment && attachment.content_type !~ /^image\/(jpeg|png|gif)$/i 
+              log "Unsupported image type '#{attachment.content_type}' for '#{image_url}'. Skipping attachment."
+              attachment.close
+              attachment = nil
+            end
+          end
+
+          send_notification(post_params, attachment)
         end
       end
     end
@@ -118,10 +147,35 @@ module Agents
       last_receive_at && last_receive_at > interpolated['expected_receive_period_in_days'].to_i.days.ago && !recent_error_logs?
     end
 
-    def send_notification(post_params)
-      response = HTTParty.post(API_URL, query: post_params)
-      puts response
+    def send_notification(post_params, attachment = nil)
+      if attachment
+        # Replace % character with its URL-encoded representation in the message
+        post_params['message'] = post_params['message'].gsub('%', ' percent')
+        
+        options = { 
+          query: post_params.except(:attachment).deep_merge({ 
+            message: CGI.escape(post_params['message']) 
+          }),
+          body: { attachment: attachment },
+          detect_mime_type: true
+        }
+        log "Sending request with attachment using HTTMultiParty"
+        log "Options: #{options.inspect}"
+        response = HTTMultiParty.post(API_URL, options)
+      else
+        response = HTTParty.post(API_URL, query: post_params)
+        log "Sending request without attachment using HTTParty"
+        log "Query parameters: #{post_params.inspect}"
+      end
+
+      log "Response status: #{response.code}"
+      log "Response body: #{response.body}"
       log "Sent the following notification: \"#{post_params.except('token').inspect}\""
+    ensure
+      attachment.close if attachment
     end
+
+
   end
 end
+
